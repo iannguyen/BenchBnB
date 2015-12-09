40cd22c2cf76a6f274dc6ec6b6eded9c3a3fa942
Ian Nguyen <iannknguyen@gmail.com>
2015-12-09 13:59:19 -0800

    change state to props

--- app/assets/javascripts/components/index.js.jsx
+++ app/assets/javascripts/components/index.js.jsx
@@ -3,35 +3,18 @@
 
   root.Index = React.createClass({
 
-    getInitialState: function() {
-      return ({benches: BenchStore.all()});
-    },
-
-    componentDidMount: function() {
-      BenchStore.addChangeListener(this._onChange);
-      ApiUtil.fetchBenches();
-    },
-
-    componentWillUnmount: function() {
-      BenchStore.removeChangeListener(this._onChange);
-    },
-
-    _onChange: function() {
-      this.setState({benches: BenchStore.all()});
-    },
-
     render: function() {
       return (
         <div className='description'>
           <ul>
-            {this.state.benches.map(function(bench) {
+            {this.props.benches.map(function(bench) {
               return (
                 <div>
                   <li>-{bench.description}-</li>
                 </div>
               );
             })
-}
+          }
           </ul>
         </div>
       );
--- app/assets/javascripts/components/search.js.jsx
+++ app/assets/javascripts/components/search.js.jsx
@@ -4,6 +4,23 @@
   root.Search = React.createClass({
     mixins: [ReactRouter.History],
 
+    getInitialState: function() {
+      return ({benches: BenchStore.all()});
+    },
+
+    componentDidMount: function() {
+      BenchStore.addChangeListener(this._onChange);
+      ApiUtil.fetchBenches();
+    },
+
+    componentWillUnmount: function() {
+      BenchStore.removeChangeListener(this._onChange);
+    },
+
+    _onChange: function() {
+      this.setState({benches: BenchStore.all()});
+    },
+
     handleMapClick: function(coordinates) {
       this.props.history.pushState(null, "/benches/new", coordinates);
     },
@@ -12,7 +29,7 @@
       return (
         <div>
           <Map handleMapClick={this.handleMapClick}/>
-          <Index/>
+          <Index benches={this.state.benches}/>
         </div>
       );
     }
